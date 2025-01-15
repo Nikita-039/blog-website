90b31e8d9d57fa4f5a6eec8b8c06a057feafec51
nikita-039 <nikita.nik393@gmail.com>
2025-01-15 14:40:27 +0530

    Updated code for styling fixes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,23 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+//   ],
+//   theme: {
+//     extend: {
+//       colors: {
+//         background: "var(--background)",
+//         foreground: "var(--foreground)",
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
 /** @type {import('tailwindcss').Config} */
 export default {
+  mode: 'jit',
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -15,3 +33,6 @@ export default {
   },
   plugins: [],
 };
+
+
+
